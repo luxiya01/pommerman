aa9b0cb9cbb15cae9c83901249e32a7be01a88ec
Li <liling@kth.se>
2019-05-09 12:02:24 +0200

    Modify environment to only have 1 PyTree agent

--- src/test.py
+++ src/test.py
@@ -15,18 +15,15 @@ def main():
     # Create a set of agents (exactly four)
     agent_list = [
         #agents.PlayerAgent(agent_control="arrows"),
-        #agents.SimpleAgent(),
-        #agents.RandomAgent(),
-        #agents.SimpleAgent(),
+        agents.SimpleAgent(),
+        agents.SimpleAgent(),
+        agents.SimpleAgent(),
         #        agents.RandomAgent(),
-        PyTreeAgent(),
-        PyTreeAgent(),
-        PyTreeAgent(),
         PyTreeAgent()
         # agents.DockerAgent("pommerman/simple-agent", port=12345),
     ]
     # Make the "Free-For-All" environment using the agent list
-    env = pommerman.make('PommeFFACompetition-v0', agent_list)
+    env = pommerman.make('PommeRadioCompetition-v2', agent_list)
 
     # Run the episodes just like OpenAI Gym
     for i_episode in range(1):
